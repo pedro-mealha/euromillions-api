464bd1c4abcea001f889fd80ade5ace5899b5489
WeNeedThePoh <pedromealha@hotmail.com>
2021-12-24 01:15:27 +0000

    add terraform configs

---
+++ configs/terraform/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.13"
+
+  backend "pg" {
+    schema_name = "euromillions-api-tf-state"
+  }
+
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "heroku" {
+  email   = var.heroku_email
+  api_key = var.heroku_api_key
+}
