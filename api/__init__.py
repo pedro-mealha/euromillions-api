f5952053929b1cc9b9c47c5330663bb793226c1d
Pedro Mealha <pedro.mealha@ultimate.ai>
2023-07-19 10:26:17 +0100

    handle cors request

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,19 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from json import JSONEncoder
 from datetime import date
-from flask import Flask
 from dotenv import load_dotenv
 
 APP_ROOT = os.path.join(os.path.dirname(__file__))
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     app.json_encoder = CustomJSONEncoder
+    app.config["CORS_METHODS"] = ["GET", "OPTIONS"]
+    CORS(app)
 
     from api.controllers import bp
     app.register_blueprint(bp)
